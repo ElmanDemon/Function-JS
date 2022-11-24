46e406c3090389d958446010a6b9ef045889e9ad
ElmanDemon <tu7lhkd9w@code.edu.az>
2022-11-25 00:24:24 +0400

    Hometask

---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+// task1()
+// function task1() {
+//     let n = 55
+//     if(n % 7 == 0) {
+//         console.log("Ədəd 7ə bölünür")
+//     }
+//     else{
+//         let qaliqhisse = n % 7;
+//         let m = n - qaliqhisse;
+//         console.log(m);
+//     }
+
+// }
+
+
+// task2()
+// function task2() {
+//     let n =33;
+//     let count = 0;
+//     if(n<50){
+//         for(let i = 0; i < n; i++){
+//             if (i % 3 == 0) {
+//                 count++;
+
+//             }
+
+//         }
+//         console.log(count);
+
+//     } 
+//     return
+// }
+
+// task3()
+// function task3() {
+//     let n = 55;
+//     let count = 0;
+//     if (50 < n && n < 100) {
+//         for (let i = 0; i < n; i++) {
+//             if (i % 5 == 0) {
+//                 count++;
+//             }
+//         }
+//         console.log(count);
+//     }
+       
+// }
+
+// task4()
+// function task4() {
+//     let n = 150;
+//     let count = 0;
+//     if(n > 100){
+//         for (let i = 0; i < n; i++) {
+//             if(i % 8 == 0){
+//                 count++;
+//             }
+//         }
+//         console.log(count);
+//     }
+// }
+
+
+
+
+
+
+
+
